50553c0412e7b7a39178d7a4674fa78fca5f402a
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-12-01 10:45:50 +0400

    Day 1 - Part 2

--- day1/index.js
+++ day1/index.js
@@ -9,4 +9,26 @@ const part1 = () => {
   console.log("Part1: " + res)
 }
 
+const part2 = () => {
+  let map = {};
+  let firstRepeating; 
+
+  let parsed = data.split('\n')
+    .map(x => parseInt(x));
+
+  let prevRes = 0;
+  
+  while(firstRepeating == null) {
+    prevRes = parsed.reduce((a, b) => {
+      let sum = a + b;
+      if (firstRepeating == null && map[sum]) firstRepeating = sum;
+      map[sum] = map[sum] ? ++map[sum] : 1;
+      return sum;
+    }, prevRes);
+  }
+
+  console.log("Part2: " + firstRepeating)
+}
+
 part1(); //435
+part2(); //245
