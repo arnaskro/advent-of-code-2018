573635260ff9c0b9018aa889988c72e1744aa2bc
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-12-02 15:54:43 +0400

    Day 2 - Part 2

--- day2/index.js
+++ day2/index.js
@@ -1,8 +1,7 @@
 var fs = require('fs');
-var input = fs.readFileSync('input.txt', 'utf8');
+var data = fs.readFileSync('input.txt', 'utf8').split('\n');
 
 const part1 = () => {
-  const data = input.split('\n');
   let countForTwo = 0,
       countForThree = 0,
       map = {};
@@ -31,4 +30,22 @@ const part1 = () => {
   console.log("part1: " + (countForThree * countForTwo))
 }
 
+const part2 = () => {
+  let map = {}
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].length; j++) {
+      // remove 1 char from the id
+      let newId = data[i].substring(0, j) + data[i].substring(j+1, data[i].length);
+
+      // check if similar id was defined && if its matching the same char pos change
+      if (!!map[newId] && map[newId] == j) {
+        console.log("part2: " + newId);
+        return newId;
+      } else map[newId] = j;
+    }
+  }
+}
+
 part1(); // 7808
+part2(); // efmyhuckqldtwjyvisipargno
