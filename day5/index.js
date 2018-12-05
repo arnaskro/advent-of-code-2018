5807472c91dfba33da3d1e89c5950a3ab3957385
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-12-05 18:05:44 +0400

    Day 5 - Part 2

--- day5/index.js
+++ day5/index.js
@@ -1,15 +1,23 @@
-var fs = require('fs');
-var input = fs.readFileSync('test.txt', 'utf8').trim().split('');
+const fs = require('fs');
+const input = fs.readFileSync('input.txt', 'utf8').trim().split('');
+const map = {};
 
 const checkIfPolymer = (prev, curr) => 
   prev.length-1 >= 0 && 
   prev[prev.length-1] != curr && 
   prev[prev.length-1].toLowerCase() == curr.toLowerCase();
 
-const removeAdjacentPolymers = (prev, curr) => 
-  (checkIfPolymer(prev, curr) ? prev.pop() : prev.push(curr)) && prev;
+const removeAdjacentPolymers = (prev, curr) => (checkIfPolymer(prev, curr) ? prev.pop() : prev.push(curr)) && prev;
+const reactPolymer = data => data.reduce(removeAdjacentPolymers, []).join('').length;
+const shortestPolymer = data => data.reduce(findShortestPolymer, Number.MAX_SAFE_INTEGER);
 
-const part1 = () =>
-  console.log("Part1: " + input.reduce(removeAdjacentPolymers, []).join('').length);
+const findShortestPolymer = (prev, curr, i, data) =>
+  (!map[curr.toLowerCase()]) ?
+    ((map[curr.toLowerCase()] = true) && 
+    Math.min(prev, reactPolymer(data.join('').replace(new RegExp(curr, 'ig'), '').split('')))) : prev;
+
+const part1 = () => console.log("Part1: " + reactPolymer(input));
+const part2 = () => console.log("Part2: " + shortestPolymer(input));
 
 part1(); // 9462
+part2(); // 4952
