96c543e079313c21dba3832cf616ac93998640e6
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-12-14 12:58:14 +0400

    Day 12 - Part 2

--- day12/index.js
+++ day12/index.js
@@ -18,6 +18,12 @@ input.slice(1, input.length)
 const step = (state, rules) => 
 	state.split('').map((_, i) => rules[state.slice(i-2, i+3)] || '.').join('');
 
+const calcSum = (state, length) => 
+	state.split('')
+	.map((x, i) => [x, i])
+	.filter(x => x[0] == '#')
+	.reduce((p, c) => p+c[1]-length, 0);
+
 const part1 = (state, rules) => {
 	let prefix = '.'.repeat(100);
 	state = prefix + state + prefix
@@ -25,14 +31,28 @@ const part1 = (state, rules) => {
 	for (let i = 0; i < 20; i++)
 		state = step(state, rules);
 
-	return state.split('')
-		.map((x, i) => [x, i])
-		.filter(x => x[0] == '#')
-		.reduce((p, c) => p+c[1]-prefix.length, 0);
+	return calcSum(state, prefix.length);
 }	
 
+const part2 = (state, rules) => {
+	let prefix = '.'.repeat(200), // increase this number if the answer is wrong
+		removeDots = new RegExp(/^\.*|\.*$/, 'g'),
+		previousState, generation = 0;
 
+	state = prefix + state + prefix
 
+	while (true) {
+		generation++;
+		state = step(state, rules);
+		// console.log(state.replace(removeDots, '')) // Show state
+		if (state.replace(removeDots, '') == previousState) break;
+		else previousState = state.replace(removeDots, '');
+	}
+
+	let nPots = state.split('').filter(x => x == '#').length;
+	return (50000000000 - generation) * nPots + calcSum(state, prefix.length);
+}
 
 // Execution & log
 console.log("Part 1: " + part1(initialState, rules)) // 2542
+console.log("Part 2: " + part2(initialState, rules)) // 2550000000883
